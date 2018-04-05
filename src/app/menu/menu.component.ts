4fbace6b6321fe8279196c5e6c7579aba79fdfc1
Ivan Tadeo Huerta <ivantec5sem@gmail.com>
2018-04-05 16:51:00 -0500

    Primer modificacion

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,32 +1,36 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
 import { Router } from '@angular/router';
 import { User } from '../models/user';
+
 
 declare var $: any;
 declare var Materialize: any;
 @Component({
   selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styles: []
+  templateUrl: './menu.component.html'
 })
 export class MenuComponent implements OnInit, AfterViewInit {
+  public session: User;
+  public perfil: number;
+  public id_usuario: number;
 
-  public sesion: User;
-
-  constructor(private router: Router) { }
+  constructor(private router: Router, private auth: AuthService) { }
 
   ngOnInit() {
+    this.perfil = this.auth.getPerfil();
+    this.id_usuario = this.auth.getIdUsuario();
     try {
       if (localStorage.datos_usuario) {
-        this.sesion = JSON.parse(localStorage.getItem('datos_usuario'));
+        this.session = JSON.parse(localStorage.getItem('datos_usuario'));
       }
     } catch (e) {
-       this.sesion = new User();
-       Materialize.toast('Error al obtener datos de sesión!', 4000, 'red');
-     }
-
+      this.session = new User();
+      Materialize.toast('Error al obtener datos de sesión!', 4000, 'red');
+    }
 
   }
+
   ngAfterViewInit() {
     $('.collapsible').collapsible();
 
