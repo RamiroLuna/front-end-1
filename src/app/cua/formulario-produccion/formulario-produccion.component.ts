ef3e7f83bd63ac2fdb220206212a67dc7e1c111b
Ivan Tadeo Huerta <ivantec5sem@gmail.com>
2018-04-10 16:38:12 -0500

    Registro de produccion

--- src/app/cua/formulario-produccion/formulario-produccion.component.ts
+++ src/app/cua/formulario-produccion/formulario-produccion.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 
+
+declare var $: any;
+declare var Materialize: any;
+
 @Component({
   selector: 'app-formulario-produccion',
-  templateUrl: './formulario-produccion.component.html',
-  styles: []
+  templateUrl: './formulario-produccion.component.html'
 })
 export class FormularioProduccionComponent implements OnInit {
+
+  public productos: Array<any> = [
+    { producto: 'Producción de mesa vibratoria', valor: '', liberado: 0 },
+    { producto: 'Producción SORTEX', valor: '', liberado: 0 },
+    { producto: 'Produccion Polvos', valor: '', liberado: 0 },
+    { producto: 'Produccion Centrifuga PET', valor: '', liberado: 0 },
+    { producto: 'Produccion Hidrofiltro Enjuague', valor: '', liberado: 0 },
+    { producto: 'Produccion Pacas NWP', valor: '', liberado: 0 },
+    { producto: 'Produccion Pacas FEA', valor: '', liberado: 0 },
+    { producto: 'Produccion Pacas SAE', valor: '', liberado: 0 }
+  ];
 
   constructor() { }
 
   ngOnInit() {
+    setTimeout(()=>{ this.ngAfterViewInit() },200);
+  }
+
+   /* 
+   * Carga de plugins para el componente
+   */
+  ngAfterViewInit(): void {
+    $('#dia').pickadate({
+      selectMonths: true, // Creates a dropdown to control month
+      selectYears: 15, // Creates a dropdown of 15 years to control year,
+      today: '',
+      clear: 'Limpiar',
+      close: 'OK',
+      monthsFull: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+      monthsShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+      weekdaysShort: ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab'],
+      weekdaysLetter: ['D', 'L', 'M', 'M', 'J', 'V', 'S'],
+      format: 'dd/mm/yyyy',
+      closeOnSelect: false, // Close upon selecting a date,
+      onClose:  () =>{
+          // this.asignacion.dia = $('#dia').val();
+      }
+    });
+
+    $('select').material_select();
   }
 
 }
