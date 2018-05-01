0ad6e7d1369cf7a184e44c6c221420a420e8c266
Ivan Tadeo Huerta <ivantec5sem@gmail.com>
2018-05-01 18:48:59 -0500

    Mi perfil [Inestable]

--- src/app/users/perfil/perfil.component.ts
+++ src/app/users/perfil/perfil.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit, AfterViewInit, EventEmitter } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
 import { PerfilService } from './perfil.service';
 import { AuthService } from '../../auth/auth.service';
 import { User } from '../../models/user';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Catalogo } from '../../models/catalogo';
+import { Linea } from '../../models/linea';
 
 
 declare var $: any;
@@ -15,18 +17,15 @@ declare var Materialize: any;
   providers: [PerfilService]
 })
 
-export class PerfilComponent implements OnInit, AfterViewInit {
+export class PerfilComponent implements OnInit {
 
-  public cambio_password: boolean;
   public usuario: User;
-  public submitted: boolean;
-  public contrasenia_nueva: string;
-  public contrasenia_anterior: string;
-  public confirmacion_nueva: string;
-  public coincide: boolean;
-  public formCambioPassword: FormGroup;
   public mensaje_error: string;
   public loading: boolean;
+  public perfiles: Array<Catalogo> ;
+  public grupos: Array<Catalogo>;
+  public lineas: Array<Linea>;
+
 
 
   constructor(
@@ -35,27 +34,29 @@ export class PerfilComponent implements OnInit, AfterViewInit {
     private fb: FormBuilder,
     private auth: AuthService,
     private router: Router) {
-    this.contrasenia_nueva;
-    this.contrasenia_anterior;
-    this.confirmacion_nueva;
-    this.cambio_password = false;
-    this.coincide = true;
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.loading = true;
-    this.submitted = false;
-    this.formCambioPassword = this.fb.group({
-      contrasenia_nueva: new FormControl(this.contrasenia_nueva, [Validators.required]),
-      confirmacion_nueva: new FormControl(this.confirmacion_nueva, [Validators.required]),
-      contrasenia_anterior: new FormControl(this.contrasenia_anterior, [Validators.required])
-    });
+    this.lineas = [];
+    this.grupos = [];
+    this.perfiles = [];
 
     this.route.params.subscribe(params => {
       if (this.auth.getIdUsuario() != null) {
-        this.service.getPerfil(this.auth.getIdUsuario()).subscribe(result => {
+        this.service.miPerfil(this.auth.getIdUsuario()).subscribe(result => {
+          console.log('result', result)
           if (result.response.sucessfull) {
-            this.usuario = result.data.usuario;
+           
+            this.lineas =  result.data.listLineas || [];
+            this.grupos =  result.data.listGrupos || [] ;
+            this.perfiles = result.data.ListPerfiles || [];
+            this.usuario = result.data.userETAD || new User();
+            let perfiles = result.data.userETAD.perfiles.split(",").map(function (item) {
+              return parseInt(item);
+            });
+
+            this.usuario.id_perfiles = perfiles;            
             this.loading = false;
           } else {
             Materialize.toast(result.response.message, 4000, 'red');
@@ -68,67 +69,11 @@ export class PerfilComponent implements OnInit, AfterViewInit {
         );
       }
     });
-
-
-
   }
 
-  ngAfterViewInit() {
-    $('#modal_cambio_password').modal({
-      dismissible: false
-    });
-
+  regresar() {
+    $('.tooltipped').tooltip('hide');
   }
 
-  onChangePassword() {
-    this.mensaje_error = "";
-    this.confirmacion_nueva = null;
-    this.contrasenia_anterior = null;
-    this.contrasenia_nueva = null;
-    this.cambio_password = !this.cambio_password;
-    $('#modal_cambio_password').modal('open');
-  }
-
-  closeModal(event) {
-    event.preventDefault();
-    this.cambio_password = false;
-    $('#modal_cambio_password').modal('close');
-  }
-
-  comprobarPassword() {
-
-    if (this.contrasenia_nueva == this.confirmacion_nueva) {
-      this.coincide = true;
-    } else {
-      this.coincide = false;
-
-    }
-  }
-
-  changePassword() {
-    // this.submitted = true;
-    // if (this.formCambioPassword.valid) {
-    //   this.service.postChangePasword(
-    //     this.contrasenia_nueva,
-    //     this.contrasenia_anterior,
-    //     this.usuario.id_usuario
-    //   ).subscribe(result => {
-    //     this.submitted = false;
-    //     this.formCambioPassword.reset();
-    //     if (result.response.sucessfull) {
-    //       this.cambio_password = false;
-    //       Materialize.toast('Contraseña actualizada!', 4000, 'green');
-    //       $('#modal_cambio_password').modal('close');
-    //     } else {
-    //       Materialize.toast(result.response.message, 4000, 'red');
-    //     }
-    //   });
-    // }
-    // else {
-    //   Materialize.toast('Se encontrarón errores!', 4000, 'red');
-    // }
-
-
-  }
 
 }
--- src/app/users/perfil/perfil.service.ts
+++ src/app/users/perfil/perfil.service.ts
@@ -11,18 +11,8 @@ export class PerfilService {
 
   constructor(private http: HttpClient) { }
 
-  getPerfil(id_usuario:number):Observable<any>{
-     return this.http.get<User>(this.URL + '?action=getPerfil&id_usuario='+id_usuario);
-  }
-
-  postChangePasword(contrasenia_nueva:string, contrasenia_anterior:string, id_usuario:number): Observable<any>{
-    
-    const body = new HttpParams()
-    .set('action', 'changePassword')
-    .set('contraseniaNueva', contrasenia_nueva)
-    .set('contraseniaAnterior', contrasenia_anterior)
-    .set('id_usuario', ""+id_usuario);
-    return this.http.post(this.URL,body);
+  miPerfil(id_acceso:number):Observable<any>{
+     return this.http.get<User>(this.URL + '?action=miPerfil&id_usuario='+id_acceso);
   }
 
 }
