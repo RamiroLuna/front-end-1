133c26f8b0102dee1601df5deabcbad23c46df6e
Ivan Tadeo Huerta <ivantec5sem@gmail.com>
2018-05-22 17:17:49 -0500

    Reporte velocidad promedio

--- src/app/cua/rpt-velocidad-promedio/rpt-velocidad-promedio.service.ts
+++ src/app/cua/rpt-velocidad-promedio/rpt-velocidad-promedio.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { BASE_URL_SERVICE } from '../../constants';
 
 @Injectable()
 export class RptVelocidadPromedioService {
 
-  constructor() { }
+  private URL = BASE_URL_SERVICE + '/Reportes';
+
+  constructor(private http: HttpClient) { }
+
+  /*
+   * Consulta de catalogo de lineas. Its important for form of Line
+   */
+  getCatalogos(id_usuario: number): Observable<any> {
+    return this.http.get<any>(this.URL + '?action=loadCombobox&id_usuario=' + id_usuario);
+  }
+  /*
+   * Consulta reporte
+   */
+  reporteVelocidadPromedio(id_usuario: number, params:any): Observable<any> {
+    return this.http.get<any>(this.URL + '?action=reporteVelocidadPromedio&id_usuario=' + id_usuario + '&id_periodo='+params.idPeriodo +'&id_linea='+params.idLinea);
+  }
 
 }
