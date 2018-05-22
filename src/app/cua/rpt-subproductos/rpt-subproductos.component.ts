7597fd3e7773f25dff0ded0f26ad10d5d711e52e
Ivan Tadeo Huerta <ivantec5sem@gmail.com>
2018-05-22 17:37:52 -0500

    reporte Subproductos

--- src/app/cua/rpt-subproductos/rpt-subproductos.component.ts
+++ src/app/cua/rpt-subproductos/rpt-subproductos.component.ts
@@ -1,15 +1,179 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { Linea } from '../../models/linea';
+import { AuthService } from '../../auth/auth.service';
+import { Periodo } from '../../models/periodo';
+import { getTablaUtf8 } from '../../utils';
+import { RptSubproductosService } from './rpt-subproductos.service';
 
+declare var $: any;
+declare var Materialize: any;
 @Component({
   selector: 'app-rpt-subproductos',
   templateUrl: './rpt-subproductos.component.html',
-  styles: []
+  styleUrls: [ './rpt-subproductos.component.css' ],
+  providers: [RptSubproductosService]
 })
 export class RptSubproductosComponent implements OnInit {
 
-  constructor() { }
+  public loading: boolean;
+  public submitted: boolean;
+  public viewReport: boolean;
+  public formConsultaPeriodo: FormGroup;
+  public paramsBusqueda: any;
+  public lineas: Array<Linea>;
+  public tituloGrafica: string;
+  public rows: Array<any>;
+  public anios: Array<any>;
+  public meses: Array<any>;
+  public periodos: Array<Periodo>;
+
+  constructor(
+    private service: RptSubproductosService,
+    private auth: AuthService,
+    private fb: FormBuilder) { }
 
   ngOnInit() {
+
+    this.loading = true;
+    this.submitted = false;
+    this.viewReport = false;   
+    this.rows = [];
+    this.tituloGrafica = "";
+    this.paramsBusqueda = {};
+
+    this.anios = [];
+    this.meses = [];
+    this.periodos = [];
+
+    this.service.getCatalogos(this.auth.getIdUsuario()).subscribe(result => {
+
+      if (result.response.sucessfull) {
+        this.lineas = result.data.listLineas || [];
+        this.lineas = this.lineas.filter(el => el.id_linea != 6).map(el => el);
+
+        this.periodos = result.data.listPeriodos || [];
+        let tmpAnios = this.periodos.map(el => el.anio);
+        this.periodos.filter((el, index) => {
+          if (tmpAnios.indexOf(el.anio) === index) {
+            this.anios.push({ valor: el.anio });
+          }
+        });
+
+        this.loading = false;
+        this.loadFormulario();
+
+        setTimeout(() => { this.ngAfterViewHttp(); }, 200)
+      } else {
+
+        this.loading = false;
+        Materialize.toast(result.response.message, 4000, 'red');
+      }
+    }, error => {
+
+      this.loading = false;
+      Materialize.toast('Ocurrió un error en el servicio!', 4000, 'red');
+    });
+
+
+  }
+
+  loadFormulario(): void {
+    this.formConsultaPeriodo = this.fb.group({
+      idLinea: new FormControl({ value: this.paramsBusqueda.idLinea }, [Validators.required]),
+      anio: new FormControl({ value: this.paramsBusqueda.anio }, [Validators.required]),
+      idPeriodo: new FormControl({ value: this.paramsBusqueda.idPeriodo }, [Validators.required])
+    });
+
+  }
+
+  /*
+   * Carga plugins despues de cargar y mostrar objetos en el DOM cuando carga la pagina
+   */
+  ngAfterViewHttp(): void {
+    $('.tooltipped').tooltip({ delay: 50 });
+    $('select').material_select();
+  }
+
+
+  agregar() {
+    $('.tooltipped').tooltip('hide');
+  }
+
+  regresar() {
+    $('.tooltipped').tooltip('hide');
+  }
+
+  changeCombo(params: string): void {
+    this.viewReport = false;
+    if (params == 'anio') {
+      this.meses = this.periodos.filter(el => el.anio == this.paramsBusqueda.anio)
+    }
+  }
+
+  busqueda(parametrosBusqueda: any) {
+
+    this.viewReport = false;
+    this.submitted = true;
+    this.rows = [];
+
+    if (this.formConsultaPeriodo.valid) {
+
+      this.service.reporteSubproductos(this.auth.getIdUsuario(), parametrosBusqueda).subscribe(result => {
+       
+        if (result.response.sucessfull) { 
+          this.rows =  result.data.reporteMap || [];
+          this.viewReport = true;
+         
+        } else {
+
+          this.viewReport = false;
+          Materialize.toast(result.response.message, 4000, 'red');
+        }
+      }, error => {
+
+        this.viewReport = false;
+        Materialize.toast('Ocurrió un error en el servicio!', 4000, 'red');
+      });
+    } else {
+      this.viewReport = false;
+      Materialize.toast('Ingrese todos los datos para mostrar reporte!', 4000, 'red');
+    }
+  }
+
+  getTextoLinea(lineas: Array<Linea>, id_linea: number): string {
+    let el = lineas.filter(el => el.id_linea == id_linea);
+
+    if (el.length > 0) {
+      return el[0].descripcion;
+    } else {
+      return "Linea no identificada"
+    }
+  }
+
+
+  exportarExcel(): void {
+    let linkFile = document.createElement('a');
+    let data_type = 'data:application/vnd.ms-excel;';
+
+    let tabla= getTablaUtf8('tblReporte');
+   
+    linkFile.href = data_type + ', ' + tabla;
+    linkFile.download = 'ReporteSubproductos';
+
+    linkFile.click();
+    linkFile.remove();
+
+  }
+
+  getPeriodo(periodos: Array<Periodo>, id_periodo: number): string {
+    let el = periodos.filter(el => el.id_periodo == id_periodo);
+
+    if (el.length > 0) {
+      return "  " + el[0].descripcion_mes + " " + el[0].anio;
+    } else {
+      return "Linea no identificada"
+    }
   }
 
 }
