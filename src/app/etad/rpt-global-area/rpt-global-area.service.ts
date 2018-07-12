59c366fc1e0fcdffac056dd576908ed23f073743
Ivan Tadeo Huerta <ivantec5sem@gmail.com>
2018-07-12 18:31:18 -0500

    Estructura rpt-global

---
+++ src/app/etad/rpt-global-area/rpt-global-area.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { BASE_URL_SERVICE } from '../../constants';
+
+
+@Injectable()
+export class RptGlobalAreaService {
+
+  private URL = BASE_URL_SERVICE + '/Reportes';
+
+  constructor(private http: HttpClient) { }
+
+  /*
+   * Consulta de catalogo de lineas.
+   */
+  getCatalogos(id_usuario: number): Observable<any> {
+    return this.http.get<any>(this.URL + '?action=loadCombobox&id_usuario=' + id_usuario);
+  }
+  
+  /*
+   * Consulta reporte
+   */
+  reporteFallas(id_usuario: number, params:any): Observable<any> {
+    return this.http.get<any>(this.URL + '?action=reporteFallas&id_usuario=' + id_usuario + '&id_periodo='+params.idPeriodo +'&id_linea='+params.idLinea);
+  }
+
+}
