7cfae89a00b7768b5c75a6ceee1f2917dc72d9ec
Ivan Tadeo Huerta <ivantec5sem@gmail.com>
2018-07-19 13:48:38 -0500

    Avance

--- src/app/etad/conf-rpt-enlace-obj-kpi/conf-rpt-enlace-obj-kpi.component.ts
+++ src/app/etad/conf-rpt-enlace-obj-kpi/conf-rpt-enlace-obj-kpi.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-conf-rpt-enlace-obj-kpi',
   templateUrl: './conf-rpt-enlace-obj-kpi.component.html',
-  styles: []
+  styleUrls: ['conf-rpt-enlace-obj-kpi.component.css']
 })
 export class ConfRptEnlaceObjKpiComponent implements OnInit {
 
