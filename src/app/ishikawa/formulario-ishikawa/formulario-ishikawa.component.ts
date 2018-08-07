c16c724b21e24e0bf92f8c0ca550330120b7ab3f
Ivan Tadeo Huerta <ivantec5sem@gmail.com>
2018-08-07 13:04:01 -0500

    Componente de registro

---
+++ src/app/ishikawa/formulario-ishikawa/formulario-ishikawa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-ishikawa',
+  templateUrl: './formulario-ishikawa.component.html',
+  styleUrls: ['./formulario-ishikawa.component.css']
+})
+export class FormularioIshikawaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/ishikawa/ishikawa.module.ts
+++ src/app/ishikawa/ishikawa.module.ts
@@ -4,9 +4,11 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { MaterializeModule } from 'angular2-materialize';
 import { OptionsComponent } from './options/options.component';
+import { FormularioIshikawaComponent } from './formulario-ishikawa/formulario-ishikawa.component';
 
 const routesIshikawa: Routes = [
-  { path: 'opciones', component: OptionsComponent }
+  { path: 'opciones', component: OptionsComponent },
+  { path: 'opciones/formulario-ishikawa/:id', component: FormularioIshikawaComponent },
 ];
 
 @NgModule({
@@ -17,6 +19,6 @@ const routesIshikawa: Routes = [
     FormsModule,
     RouterModule.forChild(routesIshikawa)
   ],
-  declarations: [OptionsComponent]
+  declarations: [OptionsComponent, FormularioIshikawaComponent]
 })
 export class IshikawaModule { }
