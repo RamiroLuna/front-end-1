252e003d1aafda7e8c50154cf6bc4d34190efdc5
Ivan Tadeo Huerta <ivantec5sem@gmail.com>
2018-08-07 17:49:37 -0500

    Avance maqueta ishikawa

--- src/app/ishikawa/formulario-ishikawa/formulario-ishikawa.component.ts
+++ src/app/ishikawa/formulario-ishikawa/formulario-ishikawa.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
 @Component({
   selector: 'app-formulario-ishikawa',
   templateUrl: './formulario-ishikawa.component.html',
@@ -10,6 +11,14 @@ export class FormularioIshikawaComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    setTimeout(() => {
+      $('.stepper').activateStepper({
+        linearStepsNavigation: false, //allow navigation by clicking on the next and previous steps on linear steppers
+        autoFocusInput: true, //since 2.1.1, stepper can auto focus on first input of each step
+        autoFormCreation: true, //control the auto generation of a form around the stepper (in case you want to disable it)
+        showFeedbackLoader: true //set if a loading screen will appear while feedbacks functions are running
+      });
+    }, 100);
   }
 
 }
